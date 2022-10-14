ae7b1553635d09fd24956882d1b71231b1e8b3c7
mikewcm <zwang3122@gatech.edu>
2022-10-14 13:59:17 -0400

    Welcome Page Changes and Game Screen Addition
    Added a "QUIT" button to the welcome page.
    Added a game screen called "activity_game.xml" with "GameActivity.java"

--- app/src/main/java/com/example/pacman/MainActivity.java
+++ app/src/main/java/com/example/pacman/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.pacman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Sets onClick function for PLAY and QUIT button
+        //playButton onClick
+        ((Button) findViewById(R.id.playButton)).setOnClickListener(this);
+        //quitButton onClick
+        ((Button) findViewById(R.id.quitButton)).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case (R.id.playButton):
+                //Switch to Config Screen, Game Screen for now.
+                //Change GameActivity.class to Config class
+                Intent intent = new Intent(getApplication(), GameActivity.class);
+                startActivity(intent);
+                break;
+            case (R.id.quitButton):
+                //Quit game function, kills application
+                moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+                break;
+        }
     }
 }
