b5a00c848aa0202adb737fdf8a79f1905c8b28a2
mikewcm <zwang3122@gatech.edu>
2022-10-16 17:38:00 -0400

    Added input functionality during onCreate for config input
    Added ability for text to change depending on the input values of the strings in strings.xml within GameActivity.java

--- app/src/main/java/com/example/pacman/GameActivity.java
+++ app/src/main/java/com/example/pacman/GameActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +17,23 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         Button mainMenu = findViewById(R.id.mainButton);
         mainMenu.setOnClickListener(this);
+
+        //Sets text values based on input values from strings.xml
+        TextView livesText = findViewById(R.id.livesText);
+        livesText.setText(getString(R.string.lives));
+
+        TextView playerNameText = findViewById(R.id.playerNameText);
+        playerNameText.setText(getString(R.string.playerName));
+
+        TextView scoreText = findViewById(R.id.scoreText);
+        scoreText.setText(getString(R.string.score));
+
+        TextView difficultText = findViewById(R.id.difficultText);
+        difficultText.setText(getString(R.string.difficulty));
+
+        TextView roundText = findViewById(R.id.roundText);
+        roundText.setText(getString(R.string.round));
+
 
     }
 
