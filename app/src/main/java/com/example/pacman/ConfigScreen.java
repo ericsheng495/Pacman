761179b0f83fb87a3af4ed6f041eaeadcee76d42
Himanshu <hvora9@gatech.edu>
2022-10-16 20:02:22 -0400

    Sprite selector

---
+++ app/src/main/java/com/example/pacman/ConfigScreen.java
@@ -0,0 +1,39 @@
+  package com.example.pacman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+  public class ConfigScreen extends AppCompatActivity {
+    int currentSprite = 0;
+    String[] sprites = {
+            "ic_menu_edit",
+            "ic_menu_delete",
+            "ic_menu_directions"
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config_screen);
+        ImageView spritePrevious = (ImageView) findViewById(R.id.spritePrevious);
+        ImageView spriteNext = (ImageView) findViewById(R.id.spriteNext);
+        spriteNext.setClickable(true);
+        spritePrevious.setClickable(true);
+        spriteNext.setOnClickListener((view) -> setPlayerSprite(1));
+        spritePrevious.setOnClickListener((view) -> setPlayerSprite(-1));
+    }
+
+    void setPlayerSprite(int next) {
+        currentSprite += next;
+        if (currentSprite < 0) {
+            currentSprite = 2;
+        } else {
+            currentSprite = currentSprite%3;
+        }
+
+        ImageView sprite = (ImageView) findViewById(R.id.playerSprite);
+        sprite.setImageResource(getResources().getIdentifier(sprites[currentSprite], "drawable", getPackageName()));
+    }
+}
